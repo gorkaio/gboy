07354d9ca2cf2e4ef3252651a218702ae9d873ea
Gorka López de Torre <gorka@gorka.io>
2019-11-02 09:57:16 +0100

    Add POP opcodes tests

--- pkg/cpu/opcodes_pushpop_test.go
+++ pkg/cpu/opcodes_pushpop_test.go
@@ -17,13 +17,39 @@ func TestPushPutsRegisterContentToSPMemoryAddress(t *testing.T) {
 
 	for _, test := range(tests) {
 		testDescription := testDescription{
-			"PUSH BC puts register content into SP memory address",
+			"PUSH puts register content into SP memory address",
 			test.opc,
 			regMap{"SP": 0x1234, test.srcReg: 0x5670},
 			regMap{"SP": 0x1232},
 			memMap{},
 			memMap{0x1232: 0x70, 0x1233: 0x56},
 			16,
+		}
+		testCase := buildTestCase(testDescription)
+		testCase.Run(t)
+	}
+}
+
+func TestPopPutsSPMemoryAddressContentIntoRegister(t *testing.T) {
+	tests := []struct{
+		opc opcode
+		dstReg string
+	}{
+		{opcode{0xC1}, "BC"},
+		{opcode{0xD1}, "DE"},
+		{opcode{0xE1}, "HL"},
+		{opcode{0xF1}, "AF"},
+	}
+
+	for _, test := range(tests) {
+		testDescription := testDescription{
+			"POP puts SP memory address content into register",
+			test.opc,
+			regMap{"SP": 0x1232},
+			regMap{"SP": 0x1234, test.dstReg: 0x5670},
+			memMap{0x1232: 0x70, 0x1233: 0x56},
+			memMap{},
+			12,
 		}
 		testCase := buildTestCase(testDescription)
 		testCase.Run(t)
